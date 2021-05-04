51ecc248d649340bc73e8c7066f1e0ae684cbb91
luisangelc <la.castillo09@info.uas.edu.mx>
2021-05-04 15:42:31 -0600

    primer comit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,26 +4,27 @@ import { Routes, RouterModule } from '@angular/router';
 import { ValidarTokenGuard } from './guards/validar-token.guard';
 
 const routes: Routes = [
-
   {
     path: 'auth',
-    loadChildren: () => import('./auth/auth.module').then( m => m.AuthModule )
+    loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule)
   },
   {
     path: 'dashboard',
-    loadChildren: () => import('./protected/protected.module').then( m => m.ProtectedModule ),
-    canActivate: [ ValidarTokenGuard ],
-    canLoad: [ ValidarTokenGuard ]
+    loadChildren: () => import('./protected/protected.module').then(m => m.ProtectedModule),
+    canActivate: [ValidarTokenGuard],
+    canLoad: [ValidarTokenGuard]
   },
   {
     path: '**',
     redirectTo: 'auth'
   }
-
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  //imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, {
+    useHash: false,
+  })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
